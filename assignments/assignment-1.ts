46a1432f11c909c4b6d4b0b9de8dff2ecbcaba4c
nareshkumar212 <nareshmulukutla@fynd-external.com>
2026-03-09 11:51:36 +0530

    assigment-1

---
+++ assignments/assignment-1.ts
@@ -0,0 +1,81 @@
+//1. Temperature of a city in degrees Celsius: 25.5
+let temp: number = 25.5;
+console.log(temp);
+
+//2. Whether a customer has placed an order: true or false
+let isPlaced : boolean = true;
+console.log(isPlaced);
+
+//3. Person's phone number: "123-456-7890"
+let phoneNum: String = "874-996-6499";
+console.log(phoneNum);
+
+//4. Amount of money in a customer's bank account: 1000.50
+let balance: number = 1000.50;
+console.log(balance);
+
+//5. Person's email address: "john.doe@example.com"
+let email: string = "john.doe@example.com";
+console.log(email);
+
+//6. Coordinates of a location (latitude, longitude): 37.7749, -122.4194
+let loc: number[] = [37.7749, -122.4194];
+console.log(loc);
+
+//7. Person's marital status: true or false
+let maritalStatus: boolean = false;
+console.log(maritalStatus);
+
+//8. Person's occupation: "Software Engineer"
+let job: string = "software Engineer";
+console.log(job);
+
+//9. Person's favourite colour: "Blue"
+let favColor: string = "Blue";
+console.log(favColor);
+
+//10. Current year: 2023
+let currentDate = new Date();
+console.log(currentDate.getFullYear()); 
+let currentYear: number = 2023;
+console.log(currentYear);
+
+//11. Number of followers on a social media platform: 1,000,000
+let followers: number = 1000000;
+console.log(followers);
+
+// 12.Rating of a movie: 7.5
+let rating: number = 7.5;
+console.log(rating);
+
+// 13.Person's blood type: 'A'
+let bloodType: string = 'A';
+console.log(bloodType);
+
+// 14.Title of a book: "To Kill a Mockingbird"
+let book: string = "To Kill a Mockingbird";
+console.log(book);
+
+// 15.Number of employees in a company: 500
+let numOfEmps: number = 500;
+console.log(numOfEmps);
+
+// 16.Time of an event: 2:30 PM
+let eventTime: string = "2:30 PM";
+console.log(eventTime);
+
+// 17.Name of a country: "United States"
+let country: string = "United States";
+console.log(country);
+
+// 18.Person's eye color: "Brown"
+let eyeColor: string = "Brown";
+console.log(eyeColor);
+
+// 19.Person's birthplace: "New York City"
+let birthplace: string = "New York City";
+console.log(birthplace);
+
+// 20. Distance between two cities: 200.5
+let distance: number = 20.5;
+console.log(distance);
