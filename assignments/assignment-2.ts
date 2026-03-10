6dbe6ed5dc324dde287e2e9f7e3345f37d57c349
nareshkumar212 <nareshmulukutla@fynd-external.com>
2026-03-10 12:18:24 +0530

    Assignment-2

---
+++ assignments/assignment-2.ts
@@ -0,0 +1,103 @@
+//1. Credit Score:
+
+let creditScore: number = 750;
+
+if(creditScore > 750){
+    console.log("Loan is Approved!");
+}else if(creditScore >= 650){
+    console.log("Additional Checks to be performed for Loan Approval");
+}else if(creditScore < 650){
+    console.log("Loan is Denied");
+}
+
+//2. Income:
+
+let income: number = 50000;
+
+if(creditScore > 750){
+    console.log("Loan is Approved!");
+}else if(creditScore >= 650){
+    console.log("Additional Checks to be performed for Loan Approval");
+    if(income >= 50000){
+        console.log("Loan is to be Considered");
+    }else{
+        console.log("Loan is Not Considered");
+    }
+}else if(creditScore < 650){
+    console.log("Loan is Denied");
+}
+
+//3. Employment Status:
+let isEmployed:boolean = true;
+
+if(creditScore > 750){
+    console.log("Loan is Approved!");
+}else if(creditScore >= 650){
+    console.log("Additional Checks to be performed for Loan Approval");
+    if(income >= 50000){
+        console.log("Loan is to be Considered");
+        if(isEmployed){
+            console.log("Customer is Employed Eligible for Loan")
+        }else{
+            console.log("Loan is Denied");
+        }
+    }else{
+        console.log("Loan is Not Considered");
+    }
+}else if(creditScore < 650){
+    console.log("Loan is Denied");
+}
+
+
+//4. Debt-to-Income Ratio:
+let dtiRatio:number = 39;
+
+if(creditScore > 750){
+    console.log("Loan is Approved!");
+}else if(creditScore >= 650){
+    console.log("Additional Checks to be performed for Loan Approval");
+    if(income >= 50000){
+        console.log("Loan is to be Considered");
+        if(isEmployed){
+            console.log("Customer is Employed Eligible for Loan");
+            if(dtiRatio < 40){
+                console.log("Loan is Approved!");
+            }else{
+                console.log("Loan is Denied")
+            }
+        }else{
+            console.log("Loan is Denied");
+        }
+    }else{
+        console.log("Loan is Not Considered");
+    }
+}else if(creditScore < 650){
+    console.log("Loan is Denied");
+}
+
+// Common function for Loan Eligibility
+
+function loanEligibility(customerName: string, creditScore: number, income: number, isEmployed: boolean, debtToIncomeRatio: number): void{
+
+    if(creditScore > 750){
+        console.log("Loan is Eligible for "+ customerName);
+    }else if(creditScore >= 650){
+        if(income >= 50000){
+            if(isEmployed){
+                if(debtToIncomeRatio < 40){
+                    console.log("Loan is Eligible for "+customerName);
+                }else{
+                    console.log("Loan is Denied for "+customerName);
+                }
+            }else{
+                console.log("Loan is Denied for "+customerName);
+            }
+        }else{
+            console.log("Loan is Denied for "+customerName);
+        }
+    }else if(creditScore < 650){
+        console.log("Loan is Denied for "+customerName);
+    }
+}
+
+loanEligibility("Jhon Doe", 720, 55000.0, true, 35.0);
