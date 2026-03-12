b41c0406c124342e527b28bb196817f2e9ba15a0
nareshkumar212 <nareshmulukutla@fynd-external.com>
2026-03-12 10:58:28 +0530

    Assignment - 3

--- assignments/assignment-3.ts
+++ assignments/assignment-3.ts
@@ -3,17 +3,10 @@ let marks: any[] = [75, 80, 82];
 let updatedmarks: any[] = [];
 let total: number = 0;
 
+console.log("Updated Marks:");
 for (let i: number = 0; i < marks.length; i++) {
     updatedmarks.push(marks[i] + 10);
-}
-
-console.log("Updated Marks:");
-
-for (let i: number = 0; i < marks.length; i++) {
     console.log(names[i]+": "+updatedmarks[i]);
-}
-
-for (let i: number = 0; i < marks.length; i++) {
     total = total + updatedmarks[i];
 }
 
